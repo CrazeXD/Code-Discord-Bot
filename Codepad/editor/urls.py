209d6a38705449997af4c54f2589b6d6fd27cad6
Craze <CrazeXD@github.com>
2022-07-22 10:58:36 -0700

    Added views

--- Codepad/editor/urls.py
+++ Codepad/editor/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from editor import views
+from . import views
 
 urlpatterns = [
-    path('', views.load_editor, name="editor")
+    path('', views.load_index, name="index"),
+    path('<int:pk>/editor', views.load_editor, name="editor"),
+    path('<int:pk>/details/', views.project_details, name="details")
 ]
