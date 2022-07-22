d5a72ada09fb067fe5050372b6479234156eaa99
Craze <CrazeXD@github.com>
2022-07-22 12:07:57 -0700

    Removed everything

--- Codepad/editor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.load_index, name="index"),
-    path('<int:pk>/editor', views.load_editor, name="editor"),
-    path('<int:pk>/details/', views.project_details, name="details")
-]
