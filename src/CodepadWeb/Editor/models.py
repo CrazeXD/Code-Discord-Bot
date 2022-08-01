7ed8334444892e1d3ba0c03c78eae3c8d7f2b825
= <=>
2022-08-01 13:59:16 -0700

    Setting up

--- src/CodepadWeb/Editor/models.py
+++ src/CodepadWeb/Editor/models.py
@@ -1,14 +1,2 @@
 from django.db import models
-import datetime
-
-# Create your models here.
-class User(models.Model):
-    useremail = models.CharField(max_length=254, unique=True)
-    password = models.CharField(max_length=32)
-    username = models.CharField(max_length=15, primary_key = True, unique=True)
-    acccount_creation_date = datetime.now()
-    class Meta:
-        verbose_name_plural = "Users"
-        ordering = ["username", "password", "useremail"]
-
     
--- src/CodepadWeb/Editor/views.py
+++ src/CodepadWeb/Editor/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,5 +8,9 @@ def index(request):
     return render(request, 'index.html', {})
 
 def signup(request):
+
+    return render(request, 'signup.html', {})
+    
+def home(request):
     context = {}
-    return render(signup, 'signup.html', context)
+    return render(request, 'home.html', context)
